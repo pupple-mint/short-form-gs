549a7f73a7ec4c8af811a0014d0ac938e6497d35
purplemint <cpfl300@gmail.com>
2025-09-07 14:52:20 +0900

    chore: 안쓰는 useEffect 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import { PlayIcon, PauseIcon, HeartIcon, HeartOutlineIcon, UploadIcon } from './components/Icons';
 import './App.css';
 
